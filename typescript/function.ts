5031e0c171f74c6f8194cf76a9e6921ca59f32fe
hc3001 <huangcheng3001@163.com>
2020-11-17 11:05:22 +0800

    Update function.ts

--- typescript/function.ts
+++ typescript/function.ts
@@ -40,5 +40,15 @@ function testFunc6(x: any[], ...items: any[]) {
     })
 }
 
-
+//谓词is 的类型保护
+function isString(test: any): test is string{
+    return typeof test === "string";
+}
+function example(foo: any){
+    if(isString(foo)){
+        console.log("it is a string" + foo);
+        console.log(foo.length); // string function
+    }
+}
+example("hello world")
 
