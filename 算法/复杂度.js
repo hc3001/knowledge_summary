7c90439fec688bc7f2c1f0370e2d075bb7c0358a
hcc <huangcheng3576@gmail.com>
2020-11-30 18:15:15 +0800

    update

--- 算法/复杂度.js
+++ 算法/复杂度.js
@@ -1,34 +1,34 @@
-// //o1, ㏒2n, o(n), o(n^2)
-// //二个时间复杂度先后排列，相当于相加，取时间复杂度高的为结果。
-// // o(1) + o(n) = o(n)
-// let i = 0
-// i += 1
-// for (let index = 0; index < 4; index++) {
-// 	console.log('t')
-// }
+//o1, ㏒2n, o(n), o(n^2)
+//二个时间复杂度先后排列，相当于相加，取时间复杂度高的为结果。
+// o(1) + o(n) = o(n)
+let i = 0
+i += 1
+for (let index = 0; index < 4; index++) {
+	console.log('t')
+}
 
-// //循环套循环，时间复杂度相乘
-// //o(n) * o(n) = o(n^2)
-// for (let s = 0; s < 2; s++) {
-// 	for (let t = 0; t < 2; t++) {
-// 		console.log('t')
-// 	}
-// }
+//循环套循环，时间复杂度相乘
+//o(n) * o(n) = o(n^2)
+for (let s = 0; s < 2; s++) {
+	for (let t = 0; t < 2; t++) {
+		console.log('t')
+	}
+}
 
-// //o(logN), 复杂度规则，log2N、log3N等都表示logN
-// let j = 0
-// let n = 10
-// while (j < n) {
-// 	console.log('t')
-// 	j *= 2
-// }
+//o(logN), 复杂度规则，log2N、log3N等都表示logN
+let j = 0
+let n = 10
+while (j < n) {
+	console.log('t')
+	j *= 2
+}
 
-// //栈：后进先出，可以用arr，push 再pop
-// let stack = []
-// stack.push(1)
-// stack.push(2)
-// const itme1 = stack.pop()
-// const itme2 = stack.pop()
+//栈：后进先出，可以用arr，push 再pop
+let stack = []
+stack.push(1)
+stack.push(2)
+const itme1 = stack.pop()
+const itme2 = stack.pop()
 //使用场景判断是否为有效括号，遇到左括号入栈，遇到右括号出栈，当数组为空时，则为合法
 
 let isBracketsValid = function(brackets) {
@@ -61,4 +61,35 @@ console.log('test', isBracketsValid(')()[]'))
 console.log('test', isBracketsValid(')'))
 console.log('test', isBracketsValid('([)]'))
 
-//函数调用堆栈，也就是执行上下文堆栈
+// 函数调用堆栈，也就是执行上下文堆栈
+// es6 实现stack 类，包括push、pop、peek 方法
+class Stack {
+	constructor() {
+		this.stackList = []
+	}
+	push(item) {
+		this.stackList.push(item)
+	}
+	pop(item) {
+		this.stackList.pop()
+	}
+	peek() {
+		return this.stackList[this.stackList.length - 1]
+	}
+}
+
+// 用栈数据结构，将100这个十进制数字转为二进制
+var stackNum = []
+var transformBinary = function(num) {
+	if(num) {
+		let r = num % 2
+		let z = Math.floor(num / 2)
+		stackNum.unshift(r)
+		if(z !== 0) {
+			transformBinary(z)
+		}
+		return stackNum.join('')
+	}
+}
+
+console.log(transformBinary(100))
---
+++ 算法/链表.js
@@ -0,0 +1,43 @@
+var a = { value: 'a' }
+var b = { value: 'b' }
+var c = { value: 'c' }
+var d = { value: 'd' }
+var e = { value: 'e' }
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+//添加链表
+var k = { value: 'k' }
+b.next = k
+k.next = c
+
+//删除链表(删除d链表)
+c.next = e
+
+//查找遍历链表
+let p = a
+while(p) {
+    console.log(p.value)
+    p = p.next
+}
+console.log(a)
+
+//翻转链表
+let test = function(a) {
+    let p1 = a
+    let p2 = null
+    while(p1) {
+        console.log(p1.value, p2 && p2.value)
+        let temp = p1.next
+        p1.next = p2
+        p2 = p1
+        p1 = temp
+    }
+    console.log(p2)
+    return p2
+}
+
+console.log(test(a))
