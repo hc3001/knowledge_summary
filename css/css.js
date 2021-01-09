66b71483a812a2769f22b01d3b7695553063f69c
hc <huangcheng3001@163.com>
2021-01-09 20:00:58 +0800

    fdsf

--- css/css.js
+++ css/css.js
@@ -1,3 +1,5 @@
 // box-sizing 有二个值：content-box（width 不包含border和padding） 和 border-box（width 包含border 和 padding）
 // flex 父元素上的值： flex-direaction （定义主轴方向） flex-wrap(是否换行) justify-content (定义子元素主轴上的对齐方式) align-item(定义交叉轴上的对齐方式) align-content（定义多根轴线的对齐方式）
 // flex 子元素上的值： flex（flex-grow/flex-shrink/flex-basis） align-self
+// 伪元素与伪类
+// css 1px像素适配  
