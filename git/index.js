8aed3d0e7ab1bca6a7d55b1ee8d788e37bcd6374
hcc <huangcheng3576@gmail.com>
2021-03-15 20:20:35 +0800

    update

--- git/index.js
+++ git/index.js
@@ -7,7 +7,7 @@
 
 // 4、Git 刷新远程分支 git remote update origin --prune
 
-// 5、Git 连接远程分支 git remote add origin xxxxxx
+// 5、Git 关联远程仓库 git remote add origin xxxxxx
 
 // 6、Git 显示工作目录和暂存区的状态 git status
 
@@ -23,4 +23,16 @@
 
 // 12、Git 打标签，默认打在最新提交上  git tag v1.0
 
-// 13、Git 查看远程地址 git remote -v
+// 13、Git 查看远程地址 git remote -v 
+
+// 14、Git 查看远程分支 git branch -a
+
+// 15、Git 钩子pre-commit
+
+// 16、git 拉取远程代码到本地
+    // 1、新建文件夹
+    // 2、初始化： git init
+    // 3、关联远程仓库：git remote add origin git@github.com:XXXX/nothing2.git
+    // 4、拉取远程仓库分支：git fetch --all
+    // 5、检出远程分支到本地：git checkout -b develop origin/develop
+    // 6、git add .   /   git commit -m 'test'   / git push origin
